83958b1403c13075de25e0135d1c1054ad492c87
sbcreates <drawsarahb@gmail.com>
2017-08-15 15:39:37 -0400

    Finished bank acct exercise

--- bank_acct.rb
+++ bank_acct.rb
@@ -1,15 +1,35 @@
 class BankAccount
 
-  @@interest_rate = 1.0006
+  @@interest_rate = 0.0006
   @@accounts = []
+
+  def initialize
+    @balance = 0
+  end
 
   def self.create
     new_account = BankAccount.new
     @@accounts << new_account
+    new_account
   end
 
-  def initialize
-    @balance = 0
+  def self.total_funds
+    total_accounts = 0
+    @@accounts.each do |account|
+      total_accounts += account.balance
+    end
+    total_accounts
+  end
+
+  def self.interest_time
+    @@accounts.each do |account|
+      interest = account.balance * @@interest_rate
+      account.deposit(interest)
+    end
+  end
+
+  def balance
+    @balance
   end
 
   def deposit(num)
@@ -21,3 +41,34 @@ class BankAccount
   end
 
 end
+
+sarah_acct = BankAccount.create
+april_acct = BankAccount.create
+
+puts sarah_acct.balance
+puts april_acct.balance
+
+puts BankAccount.total_funds
+
+sarah_acct.deposit(200)
+april_acct.deposit(1000)
+
+puts sarah_acct.balance
+puts april_acct.balance
+
+puts BankAccount.total_funds
+
+BankAccount.interest_time
+
+puts sarah_acct.balance
+puts april_acct.balance
+
+puts BankAccount.total_funds
+
+sarah_acct.withdraw(50)
+april_acct.withdraw(150)
+
+puts sarah_acct.balance
+puts april_acct.balance
+
+puts BankAccount.total_funds
---
+++ book_lending.rb
@@ -0,0 +1,28 @@
+class Book
+
+  @@on_shelf = []
+  @@on_loan = []
+
+  def
+
+  #READER
+  def due_date
+  end
+
+  #WRITER
+  def due_date=
+  end
+
+  def initialize
+  end
+
+  def borrow
+  end
+
+  def return_to_library
+  end
+
+  def lent_out?
+  end
+
+  def
