1e3e28b75b85a8565aa5fb6ccfe24fe0d217fb4f
sbcreates <drawsarahb@gmail.com>
2017-08-15 12:51:04 -0400

    Did 6 & 7 of exercise

--- bank_acct.rb
+++ bank_acct.rb
@@ -3,6 +3,11 @@ class BankAccount
   @@interest_rate = 1.0006
   @@accounts = []
 
+  def self.create
+    new_account = BankAccount.new
+    @@accounts << new_account
+  end
+
   def initialize
     @balance = 0
   end
@@ -11,4 +16,8 @@ class BankAccount
     @balance += num
   end
 
-  
+  def withdraw(num)
+    @balance -= num
+  end
+
+end
